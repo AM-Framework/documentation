1ea2219f6e165f4be2586b25791e3bad7e38b826
Arctos2win <yelle01.vs@gmail.com>
2023-11-07 22:10:25 +0100

    update docs

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>AM Framework</span>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
@@ -11,7 +11,7 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Copyright © AM Framework 2023',
   },
 }
 
